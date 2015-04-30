8faa7a949934f07cba34018a13ff259d95fbf2db
Erik Schierboom <erik_schierboom@hotmail.com>
2015-04-30 13:56:21 +0200

    Improved NuGet package building

--- buildpackage.cmd
+++ buildpackage.cmd
@@ -1,1 +1,8 @@
-".nuget\nuget.exe" pack -Prop Configuration=Release ".\src\AspNetMvcDictionarySerialization\AspNetMvcDictionarySerialization.csproj" -Symbols
+REM Ensure that the build directory exists, as NuGet.exe won't create it
+if not exist ".\build" mkdir ".\build"
+
+REM Remove existing NuGet files
+DEL ".\build\*.nupkg"
+
+".\tools\nuget\nuget.exe" update -self
+".\tools\nuget\nuget.exe" pack ".\src\AspNetMvcDictionarySerialization\AspNetMvcDictionarySerialization.csproj" -Build -OutputDirectory ".\build" -Properties Configuration=Release 
